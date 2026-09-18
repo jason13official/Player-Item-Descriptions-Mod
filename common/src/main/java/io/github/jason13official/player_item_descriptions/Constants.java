b29de47ca7441221c5e26b96c8a6bb83c196c52e
jason13official <jason13official@gmail.com>
2026-09-18 17:47:37 -0400

    refactor

---
+++ common/src/main/java/io/github/jason13official/player_item_descriptions/Constants.java
@@ -0,0 +1,11 @@
+package io.github.jason13official.player_item_descriptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "player_item_descriptions";
+    public static final String MOD_NAME = "Player Item Descriptions";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/io/github/jason13official/player_item_descriptions/PlayerItemDescriptionsFabric.java
@@ -0,0 +1,18 @@
+package io.github.jason13official.player_item_descriptions;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PlayerItemDescriptionsFabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+        // This method is invoked by the Fabric mod loader when it is ready
+        // to load your mod. You can access Fabric and Common code in this
+        // project.
+
+        // Use Fabric to bootstrap the Common mod.
+        Constants.LOG.info("Hello Fabric world!");
+        PlayerItemDescriptions.init();
+    }
+}
